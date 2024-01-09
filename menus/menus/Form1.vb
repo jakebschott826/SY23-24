d7300bdab30e739ea1338a42d45a16b69840d946
jakeb <jbsch632@learner.dvusd.org>
2024-01-09 12:48:46 -0700

    menus edit

--- menus/menus/Form1.vb
+++ menus/menus/Form1.vb
@@ -1,5 +1,27 @@
 ﻿Public Class Form1
     Private Sub CloseToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles CloseToolStripMenuItem.Click
-        TextBox1.close
+
+    End Sub
+
+    Private Sub SaveToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SaveToolStripMenuItem.Click
+        Dim outfile As New IO.StreamWriter("out.txt")
+        outfile.Write(TextBox1.Text)
+        outfile.Close()
+    End Sub
+
+    Private Sub ExitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExitToolStripMenuItem.Click
+        Me.Close()
+    End Sub
+
+    Private Sub NewToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles NewToolStripMenuItem.Click
+        TextBox1.Clear()
+    End Sub
+
+    Private Sub OpenToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles OpenToolStripMenuItem.Click
+        If IO.File.Exists("out.txt") Then
+            Dim infile As New IO.StreamReader("out.txt")
+            TextBox1.Text = infile.ReadToEnd
+            infile.Close()
+        End If
     End Sub
 End Class
